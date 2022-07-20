e1c3c5e516ca3653015874b5cf82183ff1cb84df
macedo <macedo.terceiro@gmail.com>
2022-07-20 06:34:44 -0300

    FEAT - Habilitado CORS, mudança no diretório

--- consulta/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from consulta.models import Consulta
-
-# class Consultas(admin.ModelAdmin):
-#     list_display = ('id', 'dia', 'hora', 'medico')
-#     list_display_links = ('id', 'dia', 'hora', 'medico')
-#     search_fields = ('dia', 'hora', 'medico')
-    
-# admin.site.register(Consulta, Consultas)
